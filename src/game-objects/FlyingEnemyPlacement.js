de9a6d7d1f2af81d8fb5ce904209c52f64b7a210
Drew Conley <drewconley@gmail.com>
2023-03-08 18:09:48 -0700

    add the practice code for video26

---
+++ src/game-objects/FlyingEnemyPlacement.js
@@ -0,0 +1,20 @@
+import { GroundEnemyPlacement } from "./GroundEnemyPlacement";
+import { DIRECTION_LEFT } from "../helpers/consts";
+import { TILES } from "../helpers/tiles";
+import Body from "../components/object-graphics/Body";
+
+export class FlyingEnemyPlacement extends GroundEnemyPlacement {
+  constructor(properties, level) {
+    super(properties, level);
+    this.tickBetweenMovesInterval = 20;
+    this.ticksUntilNextMove = this.tickBetweenMovesInterval;
+  }
+
+  renderComponent() {
+    const frameCoord =
+      this.spriteFacingDirection === DIRECTION_LEFT
+        ? TILES.ENEMY_FLYING_LEFT
+        : TILES.ENEMY_FLYING_RIGHT;
+    return <Body frameCoord={frameCoord} yTranslate={-3} showShadow={true} />;
+  }
+}
