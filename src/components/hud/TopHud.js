3278b19c1eb06aef2bddabfe30e3686759a6a5a0
Drew Conley <drewconley@gmail.com>
2023-03-17 11:06:13 -0600

    Add video 41

--- src/components/hud/TopHud.js
+++ src/components/hud/TopHud.js
@@ -1,6 +1,7 @@
 import styles from "./TopHud.module.css";
 import FlourCount from "./FlourCount";
 import ClockCount from "./ClockCount";
+import InventoryList from "./InventoryList";
 
 export default function TopHud({ level }) {
   return (
@@ -8,6 +9,7 @@ export default function TopHud({ level }) {
       <div className={styles.topHudLeft}>
         <FlourCount level={level} />
         <ClockCount level={level} />
+        <InventoryList level={level} />
       </div>
       <div className={styles.topHudRight}>
         {/*<span>Come back to me</span>*/}
